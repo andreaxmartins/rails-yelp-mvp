0446e26b2ee5acb9dee76e2d0e278d0a269ea915
Andrea Martins <andreaxmartins@gmail.com>
2022-05-18 15:49:12 +0200

    Creates routes and controllers

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+# Generates fake data
+gem "faker"
+
 # Build forms
 gem "simple_form", github: "heartcombo/simple_form"
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,26 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # A visitor can see the list of all restaurants.
+  # GET "restaurants"
+  get '/restaurants', to: 'restaurants#index'
+
+  # A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
+  # GET 'restaurants/new'
+  get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+
+  # POST 'restaurants'
+  post '/restaurants', to: 'restaurants#create'
+
+  # A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
+  # GET 'restaurants/38'
+  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # A visitor can add a new review to a restaurant
+  # GET "restaurants/38/reviews/new"
+  get '/restaurants/:id/reviews/new', to: 'reviews#new', as: :new_review
+
+  # POST "restaurants/38/reviews"
+  post '/restaurants/:id/reviews', to: 'reviews#create'
 end
